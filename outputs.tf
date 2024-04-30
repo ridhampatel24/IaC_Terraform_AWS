5e32ac8ab94977156e50f345fa48fa0d315fa42f
Ridham <ridhampatel2002@gmail.com>
2024-04-30 15:02:36 +0530

    Updated output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-/*output "server_ec2_connection_string" {
-  value = format("%s%s","ssh -i /home/ridham/.ssh/id_rsa ubuntu@",module.server.dev_proj_1_ec2_instance_public_ip)
-}*/
+output "server_ec2_connection_string" {
+  value = format("%s%s", "ssh -i /home/ridham/.ssh/id_rsa ubuntu@", module.server.dev_proj_1_ec2_instance_public_ip)
+}

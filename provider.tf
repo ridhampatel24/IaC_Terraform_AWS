6309419ae9519390fef19c73574ecafcbb12a9a9
Ridham Patel <105585070+ridhampatel24@users.noreply.github.com>
2024-05-15 22:46:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                   = "ap-south-1"
-  shared_credentials_files = ["/home/ridham/.aws/credentials"]
+  #shared_credentials_files = ["/home/ridham/.aws/credentials"]
 }
